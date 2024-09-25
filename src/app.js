318db3d47e531652ba06ec63cd9d6b01924a9ce0
mahbub-khan <mrkict39@gmail.com>
2024-09-25 18:40:11 +0200

    Healthcheck codes added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
 //common middlewares
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-export {app}
+//import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+//routes
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+export { app };
